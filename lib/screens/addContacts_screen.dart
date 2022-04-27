8887769993710bad1ebd202dc4d1d66abd7d30f8
Ahsanul Islam <64695124+ahsanul20101525@users.noreply.github.com>
2022-04-27 07:07:37 +0600

    added backup image

--- lib/screens/addContacts_screen.dart
+++ lib/screens/addContacts_screen.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'dart:io';
+import 'dart:async';
+import 'package:flutter/services.dart' show rootBundle;
+import 'package:path_provider/path_provider.dart';
 import 'package:provider/provider.dart';
 
 import '../providers/contacts_provider.dart';
@@ -16,20 +19,39 @@ class AddContactScreen extends StatefulWidget {
 
 class _AddContactScreenState extends State<AddContactScreen> {
   final _nameController = TextEditingController();
-  var _pickedImage;
+  var _pickedImage = null;
 
   void _selectImage(File pickedImage) {
     _pickedImage = pickedImage;
   }
 
-  void _saveContact() {
-    if (_nameController.text.isEmpty || _pickedImage == null) {
-      // can add showDialog
-      return;
+  Future<File> getImageFileFromAssets(String path) async {
+    final byteData = await rootBundle.load('assets/images/$path');
+
+    final file = File('${(await getTemporaryDirectory()).path}/$path');
+    await file.writeAsBytes(byteData.buffer
+        .asUint8List(byteData.offsetInBytes, byteData.lengthInBytes));
+    await file.create(recursive: true);
+    return file;
+  }
+
+  void _saveContact() async {
+    // if (_nameController.text.isEmpty || _pickedImage == null) {
+    //   // can add showDialog
+    //   return;
+    // }
+
+    File backupImage = await getImageFileFromAssets('cont_icon.png');
+
+    if (_pickedImage == null) {
+      Provider.of<ContactsProvider>(context, listen: false)
+          .addContact(_nameController.text, backupImage);
+      Navigator.of(context).pop();
+    } else {
+      Provider.of<ContactsProvider>(context, listen: false)
+          .addContact(_nameController.text, _pickedImage);
+      Navigator.of(context).pop();
     }
-    Provider.of<ContactsProvider>(context, listen: false)
-        .addContact(_nameController.text, _pickedImage);
-    Navigator.of(context).pop();
   }
 
   @override
