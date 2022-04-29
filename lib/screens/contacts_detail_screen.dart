1c9deb0e671b853200dae98781f9b2613b1d75f0
Ahsanul Islam <64695124+ahsanul20101525@users.noreply.github.com>
2022-04-29 16:00:58 +0600

    added detail screen butttons

--- lib/screens/contacts_detail_screen.dart
+++ lib/screens/contacts_detail_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:flutter_sticky_header/flutter_sticky_header.dart';
 
 import '../providers/contacts_provider.dart';
 
@@ -40,18 +41,116 @@ class ContactDetailScreen extends StatelessWidget {
               ),
             ),
           ),
-          SliverList(
-            delegate: SliverChildListDelegate(
-              [
-                SizedBox(
-                  height: 500,
+          // SliverList(
+          //   delegate: SliverChildListDelegate(
+          //     [
+          //       SizedBox(
+          //         height: 500,
+          //       ),
+          //       Text('Hello'),
+          //       SizedBox(
+          //         height: 500,
+          //       ),
+          //     ],
+          //   ),
+          // ),
+          SliverStickyHeader(
+            header: Padding(
+              padding: const EdgeInsets.all(15.0),
+              child: Card(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10),
                 ),
-                Text('Hello'),
-                SizedBox(
-                  height: 500,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    Expanded(child: callButton()),
+                    Expanded(child: smsButton()),
+                    Expanded(child: emailButton()),
+                  ],
                 ),
-              ],
+              ),
             ),
+            sliver: SliverList(
+              delegate: SliverChildListDelegate(
+                [
+                  SizedBox(
+                    height: 500,
+                  ),
+                  Text('Hello'),
+                  SizedBox(
+                    height: 500,
+                  ),
+                  SizedBox(
+                    height: 500,
+                  ),
+                  Text('Bissho'),
+                  SizedBox(
+                    height: 500,
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget callButton() {
+    return InkWell(
+      borderRadius: BorderRadius.circular(10),
+      enableFeedback: true,
+      onTap: () {},
+      child: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.fromLTRB(0, 12, 0, 4),
+            child: Icon(Icons.phone_rounded),
+          ),
+          Padding(
+            padding: const EdgeInsets.fromLTRB(0, 4, 0, 12),
+            child: Text('Call'),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget smsButton() {
+    return InkWell(
+      borderRadius: BorderRadius.circular(10),
+      enableFeedback: true,
+      onTap: () {},
+      child: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.fromLTRB(0, 12, 0, 4),
+            child: Icon(Icons.message_rounded),
+          ),
+          Padding(
+            padding: const EdgeInsets.fromLTRB(0, 4, 0, 12),
+            child: Text('Text'),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget emailButton() {
+    return InkWell(
+      borderRadius: BorderRadius.circular(10),
+      enableFeedback: true,
+      onTap: () {},
+      child: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.fromLTRB(0, 12, 0, 4),
+            child: Icon(Icons.email_rounded),
+          ),
+          Padding(
+            padding: const EdgeInsets.fromLTRB(0, 4, 0, 12),
+            child: Text('Email'),
           ),
         ],
       ),
