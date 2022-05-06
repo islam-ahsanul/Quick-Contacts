0aed44b609f73fe4ca010ba0395b5182bde187c0
Ahsanul Islam <64695124+ahsanul20101525@users.noreply.github.com>
2022-05-06 21:31:03 +0600

    added refreshed button

--- lib/screens/tabs_screen.dart
+++ lib/screens/tabs_screen.dart
@@ -19,12 +19,51 @@ class _TabScreenState extends State<TabScreen> {
       length: 2,
       child: Scaffold(
         appBar: AppBar(
+          foregroundColor: Colors.black,
           flexibleSpace: FlexibleSpaceBar(
             background: Image.asset(
               'assets/images/background_one.jpg',
               fit: BoxFit.cover,
             ),
           ),
+          actions: [
+            PopupMenuButton(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(15),
+              ),
+              itemBuilder: (context) {
+                return [
+                  PopupMenuItem(
+                    padding: EdgeInsets.all(0),
+                    value: 0,
+                    child: Row(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 9),
+                          child: Icon(
+                            Icons.refresh_rounded,
+                            color: Colors.black,
+                          ),
+                        ),
+                        Text(
+                          'Refresh',
+                          style: TextStyle(
+                            fontSize: 18,
+                            // fontFamily: 'SourceSansPro',
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ];
+              },
+              onSelected: (value) {
+                if (value == 0) {
+                  Navigator.of(context).pushReplacementNamed('/');
+                }
+              },
+            ),
+          ],
           bottom: TabBar(
             // indicatorColor: Colors.red,
             labelStyle: TextStyle(
